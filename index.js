6f926e16c3f993223c33c92962d6fb628fffad23
Collin <candrew022@gmail.com>
2018-07-03 09:16:17 -0400

    initialize body-parser and use with express

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+
+app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
   const user = req.body.username;
